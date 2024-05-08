a4573ba03e6e8a374076133f1c0d2308a1bf7897
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-05-08 12:46:13 +0530

    Private Routing
    Make routing to dashboard private. Only after login user can able to view the dashboard.

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,4 +1,12 @@
-export default function Dashboard () {
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom";
+
+export default function Dashboard() {
+    const { user } = useSelector(state => state.user);
+    if (!user) {
+        return <Navigate to={'/sign-in'} />
+    }
+
     return (
         <h1>Dasboard</h1>
     )
