63a98dde8b69b620a4dff35cba493f35d2b41578
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-05-26 16:08:03 +0530

    Provide queryClient
    create a queryClient and wrapped the <App /> component with queryClientProvider.

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,13 +6,17 @@ import { store, persistor } from './store/index.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import ThemeProvider from './components/ThemeProvider.jsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import queryClient from './utils/http.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
         <ThemeProvider>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </ThemeProvider>
       </Provider>
     </PersistGate>
