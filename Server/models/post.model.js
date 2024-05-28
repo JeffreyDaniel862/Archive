eca3d96893e5c322fb4f441a42368713c53d5ac5
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-05-28 15:14:48 +0530

    create post model using sequelize and create relation with user table.

---
+++ Server/models/post.model.js
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import { db } from "../config/database.js";
+import User from "./user.model.js";
+
+const Post = db.define('post', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "Uncategorized"
+    },
+    coverImage: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, { tableName: 'posts', timestamps: true });
+
+(async () => {
+    await db.sync();
+})();
+
+User.hasMany(Post, { onDelete: 'cascade' });
+Post.belongsTo(User);
+
+export default Post;
