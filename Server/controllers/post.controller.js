efb08e343d18caa6db2785e838df0f0a8eb483b2
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-05-30 16:07:48 +0530

    create route and controller to get posts.

--- Server/controllers/post.controller.js
+++ Server/controllers/post.controller.js
@@ -1,19 +1,64 @@
+import { Sequelize } from "sequelize";
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/errorHandler.js"
 
 export const createPost = async (req, res, next) => {
-    if(req.user.id != req.params.id){
+    if (req.user.id != req.params.id) {
         next(errorHandler(403, "Access denied"));
     }
-    if(!req.body.title || !req.body.content){
+    if (!req.body.title || !req.body.content) {
         next(errorHandler(400, "Please fill all necessary fields to publish a blog."));
     }
 
     const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-');
-    const post = {...req.body, slug, userId: req.user.id}
+    const post = { ...req.body, slug, userId: req.user.id }
     try {
         await Post.create(post);
-        res.status(201).json({message: 'post created successfully'});
+        res.status(201).json({ message: 'post created successfully' });
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? "ASC" : "DESC";
+        const { count, rows: posts } = await Post.findAndCountAll({
+            where: {
+                ...(req.query.userId && { userId: req.query.userId }),
+                ...(req.query.category && { category: req.query.category }),
+                ...(req.query.slug && { slug: req.query.slug }),
+                ...(req.query.id && { id: req.query.id }),
+                ...(req.query.searchTerm && {
+                    [Sequelize.Op.or]: [
+                        { title: { [Sequelize.Op.iRegexp]: req.query.searchTerm } },
+                        { content: { [Sequelize.Op.iRegexp]: req.query.searchTerm } },
+                    ],
+                }),
+            },
+            offset: startIndex,
+            limit,
+            order: [['createdAt', sortDirection]]
+        });
+        const now = new Date();
+        const aMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+        const totalPosts = await Post.count();
+        const lastMonthPosts = await Post.count({
+            where: {
+                createdAt: {
+                    [Sequelize.Op.gt]: aMonthAgo
+                },
+            },
+        });
+
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+
     } catch (error) {
         next(error)
     }
--- Server/routes/post.route.js
+++ Server/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import verifyUser from '../utils/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-router.post('/create-post/:id', verifyUser, createPost)
+router.post('/create-post/:id', verifyUser, createPost);
+router.get('/getposts', getPosts)
 
 export default router;
