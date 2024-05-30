f092ccb93fdceac8e4af29e6b1f4101f44f62f82
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-05-30 17:04:34 +0530

    create fetchPost to get user's post from the server using useQuery.

--- Client/src/components/DashPosts.jsx
+++ Client/src/components/DashPosts.jsx
@@ -1,9 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 const DashPosts = () => {
+    const { user } = useSelector(state => state.user);
+    const [postData, setPostData] = useState(undefined);
+    const { data, isLoading, isError, error, isSuccess } = useQuery({
+        queryKey: ['getPosts'],
+        queryFn: () => fetchPost(user?.id),
+        staleTime: 5000
+    });
+
+    useEffect(() => {
+        if(isSuccess){
+            setPostData(data);
+        }
+    }, [data])
+
     return (
         <div>
             Dash Posts.
+            {
+                postData && postData.posts.map(post => <p key={post.id}>{post.title}</p>)
+            }
         </div>
     )
 }
 
 export default DashPosts;
+
+export const fetchPost = async (id) => {
+    try {
+        const response = await fetch(`/jd/post/getposts?userId=${id}`);
+        const resData = await response.json()
+        return resData;
+    } catch (error) {
+        console.log(error);
+    }
+}
