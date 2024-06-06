2e6d967be45c2647bed55b418b701ea1a23b3467
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-06 13:50:26 +0530

    create post card for dashboard.

--- Client/src/components/DashPosts.jsx
+++ Client/src/components/DashPosts.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Card from './Card';
+import { FaTrash, FaEdit  } from "react-icons/fa";
 
 const DashPosts = () => {
     const { user } = useSelector(state => state.user);
@@ -22,8 +23,22 @@ const DashPosts = () => {
         <div>
             Dash Posts.
             {
-                postData && <div className='flex flex-col md:flex-row md:flex-wrap gap-3 p-3'>
-                    {postData.posts.map(post => <Card key={post.id} post={post} />)}
+                postData && <div className='flex flex-col md:flex-row md:flex-wrap gap-6 p-3'>
+                    {postData.posts.map(post =>
+                        <div className='flex flex-col gap-3 p-2 rounded-lg h-[410px]' key={post.id} >
+                            <Card post={post} />
+                            <div className='flex justify-around'>
+                                <button className='flex gap-3 justify-center items-center p-2 border-red-700 border-2 rounded-lg hover:bg-red-800 hover:text-white hover:-translate-y-1 transition-all'>
+                                    <FaTrash />
+                                    <span>Delete</span>
+                                </button>
+                                <button className='flex gap-3 justify-center items-center p-2 border-green-700 border-2 rounded-lg hover:bg-green-600 hover:text-white hover:-translate-y-1 transition-all'>
+                                    <FaEdit  />
+                                    <span>Edit</span>
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             }
         </div>
