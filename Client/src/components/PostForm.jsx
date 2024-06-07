4b29c42e1864fc6062b2665ca939345979ce28e6
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-07 17:04:28 +0530

    Update postform.
    Update postform to handle both creation and updation of blog posts.

--- Client/src/components/PostForm.jsx
+++ Client/src/components/PostForm.jsx
@@ -1,5 +1,5 @@
 import { Alert, Button, Select, TextInput } from "flowbite-react";
-import { Form, redirect, useActionData, useSubmit } from "react-router-dom";
+import { Form, json, redirect, useActionData, useSubmit } from "react-router-dom";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
@@ -7,16 +7,20 @@ import { useSelector } from 'react-redux';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import app from "../firebase";
 
-export default function PostForm({ title }) {
+export default function PostForm({ title, method, postData }) {
   const { user } = useSelector(state => state.user);
-  const [formData, setFormData] = useState({ userId: user?.id });
+  const [formData, setFormData] = useState(undefined);
   const quill = useRef();
   const submit = useSubmit();
   const data = useActionData();
+  const coverImageRef = useRef(null);
+  const [imageUpload, setImageUpload] = useState(false);
 
   useEffect(() => {
-    if (data && data.statusCode != 201) {
-
+    if (postData) {
+      const { createdAt, updatedAt, slug, ...data } = postData;
+      console.log(data);
+      setFormData(data);
     }
   }, [])
 
@@ -78,8 +82,9 @@ export default function PostForm({ title }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
-    submit(formData, { method: 'POST' });
+    const submitData = { ...formData, userId: user?.id }
+    console.log(submitData);
+    submit(submitData, { method: method });
   }
 
   const handleChange = (event) => {
@@ -131,17 +136,25 @@ export default function PostForm({ title }) {
   );
 
   const handleCoverImage = async (event) => {
+    setImageUpload(true)
     const coverImage = await handleImageUpload(event.target.files[0]);
     setFormData(prev => ({ ...prev, coverImage }));
+    setImageUpload(false)
+  }
+
+  let content = "Upload cover image"
+
+  if (formData && formData.coverImage) {
+    content = "Change cover image"
   }
 
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">{title}</h1>
-      <Form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      <Form method={method} className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row gap-3">
-          <TextInput name="title" required className="flex-1" type="text" placeholder="Title" onChange={handleChange} />
-          <Select name="category" required onChange={handleChange}>
+          <TextInput defaultValue={formData ? formData.title : ''} name="title" required className="flex-1" type="text" placeholder="Title" onChange={handleChange} />
+          <Select value={formData ? formData.category : ''} name="category" required onChange={handleChange}>
             <option value="holder">Select a category</option>
             <option value="javascript">JavaScript</option>
             <option value="react.js">React.js</option>
@@ -151,39 +164,53 @@ export default function PostForm({ title }) {
             <option value="git">Git</option>
           </Select>
         </div>
-        <div className="flex gap-3 justify-between items-center w-full border-4 p-3 border-teal-700 border-dashed hover:border-double rounded-md">
-          <input required type="file" accept="image/*" className="rounded-sm" onChange={handleCoverImage} />
-          <Button gradientDuoTone={'greenToBlue'} outline>Upload</Button>
+        <div className="flex flex-col gap-3 justify-between items-center w-full border-4 p-3 border-teal-700 border-dashed hover:border-double rounded-md">
+          {formData && formData.coverImage && <img className="w-full h-60 object-cover rounded-lg" src={formData.coverImage} alt="cover image" />}
+          <input type="file" ref={coverImageRef} accept="image/*" className="rounded-sm hidden" onChange={handleCoverImage} />
+          <Button disabled={imageUpload} className="self-end" gradientDuoTone={'greenToBlue'} onClick={() => coverImageRef.current.click()} outline>{imageUpload ? "Uploading" : content}</Button>
         </div>
-        <ReactQuill onChange={value => setFormData(prev => ({ ...prev, content: value }))} name='content' formats={formats} modules={modules} ref={el => quill.current = el} theme="snow" className="h-72 mb-12" required placeholder="Your content please ..." />
-        <Button type="submit" className=" mt-4 bg-gradient-to-r from-blue-600 to-sky-500">Publish</Button>
+        <ReactQuill value={formData ? formData.content : ''} onChange={value => setFormData(prev => ({ ...prev, content: value }))} name='content' formats={formats} modules={modules} ref={el => quill.current = el} theme="snow" className="h-72 mb-12" required placeholder="Your content please ..." />
+        <Button type="submit" className=" mt-4 bg-gradient-to-r from-blue-600 to-sky-500">{method === "POST" ? "Publish Writ" : "Update Writ"}</Button>
       </Form>
       {
-        data && data.statusCode != 201 && <Alert color={'failure'} className="mt-4">{data?.message || 'Unable to post blog'}</Alert>
+        data && <Alert color={'failure'} className="mt-4">{data?.message || 'Unable to post blog'}</Alert>
       }
     </div>
   )
 }
 
 export const postAction = async ({ request }) => {
+  const method = request.method
   const data = await request.formData();
   const postInfo = {}
   data.forEach((value, key) => postInfo[key] = value);
   const { userId, ...remain } = postInfo;
   try {
-    const response = await fetch('/jd/post/create-post/' + userId, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(postInfo)
-    });
-
-    if (response.ok) {
-      return redirect('/dashboard?tab=profile')
+    let response;
+    if (method === "POST") {
+      response = await fetch('/jd/post/create-post/' + userId, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postInfo)
+      });
     }
-    return response;
+    if (method === "PUT") {
+      response = await fetch(`/jd/post/update/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postInfo)
+      });
+    }
+    const resData = await response.json();
+    if (response.ok) {
+      return redirect('/dashboard?tab=posts');
+    }
+    return resData;
   } catch (error) {
-    console.log(error);
+    throw json({ message: "Bad request" }, { status: 500 });
   }
 }
