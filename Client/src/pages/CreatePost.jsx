199d9dcca6ff398f21371ff30e6565b8c19ffe70
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-07 14:45:53 +0530

    create CreatePost page.
    CreatePost page returns PostForm component if the user is logged-in or else redirects the user to sign-in page.

---
+++ Client/src/pages/CreatePost.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import PostForm from "../components/PostForm";
+import SignIn from "./SignIn";
+
+export default function CreatePost() {
+    const { user } = useSelector(state => state.user);
+
+    return user ? <PostForm title={'Create Post'} /> : <SignIn />
+}
