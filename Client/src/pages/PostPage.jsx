11fb1e95ec1cf09e766d63b05a121a3f1b62c573
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-08 16:04:26 +0530

    Update PostPage to show post's author information.

--- Client/src/pages/PostPage.jsx
+++ Client/src/pages/PostPage.jsx
@@ -1,19 +1,37 @@
 import { useLoaderData } from "react-router-dom";
 import { Button } from 'flowbite-react'
+import { useEffect, useState } from "react";
 
 export default function PostPage() {
     const post = useLoaderData();
-    
+    const [author, setAuthor] = useState(undefined);
+    useEffect(() => {
+        if (post) {
+            const fetchUser = async () => {
+                const response = await fetch(`/jd/user/getUser/${post.userId}`);
+                const resData = await response.json();
+                if (response.ok) {
+                    setAuthor(resData)
+                }
+            }
+            fetchUser();
+        }
+    }, [post]);
+
     return (
         <main className="p-3 flex flex-col gap-3 items-center max-w-6xl min-h-screen mx-auto">
             <h1 className="text-3xl font-serif lg:text-4xl max-w-2xl mx-auto mt-10">{post.title}</h1>
             <Button className="mt-5" pill color={'gray'} size={'xs'}>{post && post.category}</Button>
             <img className="w-full max-h-[660px] object-cover mt-10 rounded-lg" src={post.coverImage} alt="cover image" />
             <div className="flex justify-between  mx-auto p-3 text-sm w-full border-b-2 border-slate-500 italic">
-                <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+                <div className="flex gap-3 justify-center items-center">
+                    <img className="h-10 w-10 rounded" src={author?.displayPicture} alt="Author's Picture" />
+                    <p className="italic text-lg">{author?.username}</p>
+                </div>
+                <p><span className="px-1">Published On:</span> {new Date(post.createdAt).toDateString()}</p>
                 <span>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
             </div>
-            <article className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}>
+            <article className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content }}>
 
             </article>
         </main>
