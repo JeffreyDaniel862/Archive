59a5986aed96799d81dcfece36f95bf56304838a
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-11 15:09:44 +0530

    Create ScrollToTop component and add to RootLayout page.

--- Client/src/pages/RootLayout.jsx
+++ Client/src/pages/RootLayout.jsx
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header'
 import { Outlet } from "react-router-dom";
+import ScrollToTop from '../components/ScrollToTop';
 
 export default function RootLayout() {
     return (
         <>
             <Header />
+            <ScrollToTop />
             <main>
                 <Outlet />
             </main>
