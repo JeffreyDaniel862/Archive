4e0c851e625b81d6172ee2ef705b8ff3e942fba7
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-12 12:38:02 +0530

    Add postComment function to Comment component and add Comment component to PostPage.

--- Client/src/components/Comment.jsx
+++ Client/src/components/Comment.jsx
@@ -1,10 +1,26 @@
-import { Button, Textarea } from 'flowbite-react';
-import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { Alert, Button, Textarea } from 'flowbite-react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 export default function Comment({ postId }) {
     const { user } = useSelector(state => state.user);
     const [comment, setComment] = useState("");
+    const { mutate, data } = useMutation({
+        mutationFn: postComment
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        mutate({ userId: user.id, postId, content: comment });
+    }
+
+    useEffect(() => {
+        if (data && data.statusCode == 200) {
+            setComment("")
+        }
+    }, [data]);
+
     return user ?
         <>
             <div className='flex gap-2 items-center text-sm text-gray-400'>
@@ -12,7 +28,7 @@ export default function Comment({ postId }) {
                 <img className='h-7 w-7 rounded-full' src={user.displayPicture} alt="display picture" />
                 <Link className='text-sky-600 dark:text-sky-400' to={'/dashboard?tab=profile'}>@{user.username}</Link>
             </div>
-            <form className='mt-5 border border-sky-300 dark:border-sky-700 p-3 rounded-md'>
+            <form onSubmit={handleSubmit} className='mt-5 border border-sky-300 dark:border-sky-700 p-3 rounded-md'>
                 <Textarea
                     placeholder='Add comments about this post ...'
                     onChange={(e) => setComment(e.target.value)}
@@ -21,8 +37,11 @@ export default function Comment({ postId }) {
                 />
                 <div className='mt-4 flex justify-between items-center'>
                     <p className='text-gray-400'>{200 - comment.length} characters remaining.</p>
-                    <Button outline pill className='bg-gradient-to-r from-blue-600 via-sky-500 to-teal-300'>Submit</Button>
+                    <Button type='submit' outline pill className='bg-gradient-to-r from-blue-600 via-sky-500 to-teal-300'>Submit</Button>
                 </div>
+                {
+                   data && data?.statusCode != 200 && <Alert className='mt-5' color={'failure'}>{data?.message || 'Unable to post comment'}</Alert>
+                }
             </form>
         </>
         :
@@ -30,4 +49,20 @@ export default function Comment({ postId }) {
             <p>Please login to comment --&gt;</p>
             <Link className='hover:underline' to={'/sign-in'}>Sign-in</Link>
         </div>
+}
+
+export const postComment = async (comment) => {
+    try {
+        const response = await fetch('/jd/comment/create', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(comment)
+        });
+        const resData = await response.json();
+        return resData;
+    } catch (error) {
+        return error;
+    }
 }
--- Client/src/pages/PostPage.jsx
+++ Client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import { Button } from 'flowbite-react'
 import { useEffect, useState } from "react";
+import Comment from "../components/Comment";
 
 export default function PostPage() {
     const post = useLoaderData();
@@ -23,9 +24,9 @@ export default function PostPage() {
             <h1 className="text-3xl font-serif lg:text-4xl max-w-2xl mx-auto mt-10">{post.title}</h1>
             <Button className="mt-5" pill color={'gray'} size={'xs'}>{post && post.category}</Button>
             <img className="w-full max-h-[660px] object-cover mt-10 rounded-lg" src={post.coverImage} alt="cover image" />
-            <div className="flex justify-between  mx-auto p-3 text-sm w-full border-b-2 border-slate-500 italic">
+            <div className="flex justify-between items-center mx-auto p-3 text-sm w-full border-b-2 border-slate-500 italic">
                 <div className="flex gap-3 justify-center items-center">
-                    <img className="h-10 w-10 rounded" src={author?.displayPicture} alt="Author's Picture" />
+                    <img className="h-10 w-10 rounded-full" src={author?.displayPicture} alt="Author's Picture" />
                     <p className="italic text-lg">{author?.username}</p>
                 </div>
                 <p><span className="px-1">Published On:</span> {new Date(post.createdAt).toDateString()}</p>
@@ -34,6 +35,9 @@ export default function PostPage() {
             <article className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content }}>
 
             </article>
+            <div className="max-w-2xl mx-auto w-full p-3">
+                <Comment postId={post.id} />
+            </div>
         </main>
     )
 }
