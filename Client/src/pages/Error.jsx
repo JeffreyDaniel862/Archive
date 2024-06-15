efef414eec4b6e37ab4ba8b302bb812504d008d3
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-15 15:49:54 +0530

    Update Error element.

--- Client/src/pages/Error.jsx
+++ Client/src/pages/Error.jsx
@@ -20,7 +20,7 @@ export default function ErrorPage() {
         <Header />
         <main className="w-full flex flex-col p-4 my-7 items-center justify-center">
             <div className="border w-full flex flex-col justify-center items-center p-4 shadow-lg rounded-lg bg-slate-600">
-                <Link className="self-start text-3xl text-red-700 transition-all hover:text-red-600 hover:scale-125"><FaTimes /></Link>
+                <FaTimes onClick={() => history.back()} className="self-start text-3xl text-red-700 transition-all hover:text-red-600 hover:scale-125" />
                 <p className="text-2xl sm:text-4xl text-red-600 my-5">{title}</p>
                 <p className="text-lg sm:text-2xl mb-4 text-red-500">{message}</p>
             </div>
