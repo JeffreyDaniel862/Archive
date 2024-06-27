ef4112b7d9218bb75e62a7d57e57eda1c5a3f41e
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-27 14:59:56 +0530

    Change height of the post container.

--- Client/src/components/DashPosts.jsx
+++ Client/src/components/DashPosts.jsx
@@ -66,7 +66,7 @@ const DashPosts = () => {
                 <h2 className='my-5 font-bold text-xl md:text-3xl text-center border-b-2 border-gray-400 pb-1'>Your Archive</h2>
                 <div className='flex flex-col self-start md:flex-row md:flex-wrap gap-6 p-3'>
                     {postData.map(post =>
-                        <div className='flex flex-col gap-3  rounded-lg h-[410px]' key={post.id} >
+                        <div className='flex flex-col gap-3  rounded-lg h-[430px]' key={post.id} >
                             <Card post={post} />
                             <div className='flex justify-around'>
                                 <button onClick={() => handleModal(post)} className='flex gap-3 justify-center items-center p-2 border-red-700 border-2 rounded-lg hover:bg-red-800 hover:text-white hover:-translate-y-1 transition-all'>
