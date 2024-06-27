b6efe0b13bf69bc504b040214e630be93af0e1c1
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-27 15:01:41 +0530

    Update Card component.
    Update card component to fetch and show author detail of the post.

--- Client/src/components/Card.jsx
+++ Client/src/components/Card.jsx
@@ -1,23 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
 import { FaRegEye } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function Card({ post }) {
+
+  const { data } = useQuery({
+    queryKey: ["AuthorOfthisPost"],
+    queryFn: () => getAuthor(post.userId),
+    enabled: Boolean(post)
+  })
+  
   return (
-    <div className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[330px] transition-all'>
+    <div className='group relative w-full shadow-md border border-sky-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[330px] transition-all'>
       <Link to={`/post-view/${post.slug}`}>
         <img
           src={post.coverImage}
           alt='post cover'
-          className='h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
+          className='h-[250px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
         />
       </Link>
       <div className='p-3 flex flex-col gap-2'>
-        <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
+        <div className='flex gap-3 items-center justify-start text-sm border-b border-gray-400 pb-2'>
+          <img className='w-8 h-8 border border-sky-600 dark:border-sky-300 rounded-full' src={data?.displayPicture} alt="author picture" />
+          <p className='font-bold'>{data?.username}</p>
+        </div>
+        <p className='text-lg font-semibold line-clamp-1'>{post.title}</p>
         <div className='flex items-center justify-between'>
           <span className='italic text-sm'>{post.category}</span>
           <div className="flex gap-3 items-center">
             <FaRegEye className="text-xl  text-sky-600 dark:text-sky-400" />
-            <span className="text-sm italic">{post && post.View.views} views</span>
+            <span className="text-sm italic">{post && post.View ? post.View.views : post.views} views</span>
           </div>
         </div>
         <Link
@@ -29,4 +41,17 @@ export default function Card({ post }) {
       </div>
     </div>
   );
+}
+
+const getAuthor = async (id) => {
+  try {
+    const response = await fetch(`/jd/user/getUser?userId=${id}`);
+    const resData = await response.json();
+    if (!response.ok) {
+      throw resData;
+    }
+    return resData;
+  } catch (error) {
+    console.error(error);
+  }
 }
