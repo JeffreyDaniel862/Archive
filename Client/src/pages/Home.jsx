cb9f6b4ed583c03c335746fd201a1ac9c9f46e37
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-06-27 15:03:23 +0530

    1st version of home page.
    Make home page to show posts of authors the user follows. if no one then shows some random post.

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,5 +1,40 @@
-export default function Home () {
+import { useSelector } from 'react-redux';
+import { useQuery } from '@tanstack/react-query'
+import Card from '../components/Card';
+
+export default function Home() {
+
+    const { user } = useSelector(state => state.user);
+
+    const { data: posts } = useQuery({
+        queryKey: ['personalizedFeed'],
+        queryFn: () => getPersonalizedFeed(user?.id),
+        enabled: Boolean(user)
+    });
+
     return (
-        <h1>Home</h1>
+        <div className='p-4 flex flex-col items-center min-h-screen'>
+            <h1 className='text-2xl lg:text-4xl border-b-2 font-semibold mb-8'>Welcome to <span className='font-bold text-sky-600 dark:text-sky-400 font-serif'>Archive</span></h1>
+            <div className='flex gap-6 flex-wrap'>
+                {
+                    posts && posts.map(post => <Card key={post.id} post={post} />)
+                }
+            </div>
+        </div>
     )
+}
+
+const getPersonalizedFeed = async (id) => {
+    try {
+        const response = await fetch(`/jd/post/getPersonalizedFeed/${id}`);
+        const resData = await response.json();
+        if (!response.ok) {
+            throw resData
+        }
+        return resData
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+
 }
