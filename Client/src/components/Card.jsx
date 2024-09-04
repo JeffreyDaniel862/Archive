285270df33e25d59a964c662a36ea4635386e355
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-09-04 20:18:06 +0530

    Minor changes in Card and Header components.
    Change the styling of card component. Change the appearance of header component when the search bar is on focus and set it to actual appearance when search bar is not in focus.

--- Client/src/components/Card.jsx
+++ Client/src/components/Card.jsx
@@ -11,7 +11,7 @@ export default function Card({ post }) {
   })
   
   return (
-    <div className='group relative w-full shadow-md border border-sky-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[330px] transition-all'>
+    <div className='group relative w-full shadow-md hover:shadow-lg hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[330px] transition-all'>
       <Link to={`/post-view/${post.slug}`}>
         <img
           src={post.coverImage}
@@ -34,7 +34,7 @@ export default function Card({ post }) {
         </div>
         <Link
           to={`/post-view/${post.slug}`}
-          className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
+          className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
         >
           Read article
         </Link>
--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -7,11 +7,14 @@ import { toggleTheme } from '../store/themeSlice';
 import { logout } from '../store/userSlice';
 import { MdDashboard } from "react-icons/md";
 import { HiCollection } from 'react-icons/hi'
+import { useState } from 'react';
 
 export default function Header() {
     const path = useLocation().pathname;
     const { user } = useSelector(state => state.user);
-    const { theme } = useSelector(state => state.theme)
+    const { theme } = useSelector(state => state.theme);
+    const [searchFocus, setSearchFocus] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const handleSignOut = async () => {
@@ -24,55 +27,74 @@ export default function Header() {
         }
     }
 
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm);
+        
+    }
+
+    const handleSearch = () => {
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(searchTerm.trim().length == 0){
+            setSearchFocus(false);
+        }
+    }
+
     return <Navbar>
         <Link to={'/'} className='self-center whitespace-nowrap text-sm sm:text-xl dark:text-white font-semibold font-serif'>
             <span className='px-2 py-1 bg-gradient-to-r from-blue-600 via-sky-500 to-teal-300 rounded-md font-serif text-white'>Arc</span>
             hive
         </Link>
-        <form>
-            <TextInput type='text' placeholder='Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline' />
+        <form className='flex gap-2 transition-all'>
+            {searchFocus && <TextInput onChange={handleSearchTermChange} type='text' placeholder='Search...' className={`${searchFocus ? "" : ""} lg:inline`} />}
+            <button onClick={!searchFocus ? () => setSearchFocus(true) : handleSubmit} className='group border p-1 rounded-full w-10 h-10 flex items-center justify-center' type='button'>
+                <AiOutlineSearch className='group-hover:scale-125' />
+            </button>
         </form>
-        <Button className='w-12 h-10 lg:hidden' color={'gray'} pill>
-            <AiOutlineSearch />
-        </Button>
-        <div className='flex gap-3 md:order-2'>
-            <Button onClick={() => { dispatch(toggleTheme()) }} className='w-12 h-10 sm:inline' color={'gray'} pill>
-                {theme == 'light' ? <FaMoon /> : <FaSun />}
-            </Button>
-            {
-                user ?
-                    <Dropdown arrowIcon={false} inline label={<Avatar alt='displaypicture' img={user.displayPicture} rounded />} >
-                        <Dropdown.Header>
-                            <span className='block text-sm'>
-                                {user.username}
-                            </span>
-                            <span className='block text-sm font-semibold truncate'>
-                                {user.email}
-                            </span>
-                        </Dropdown.Header>
-                        <Link to={'/dashboard?tab=profile'}>
-                            <Dropdown.Item className='text-sm font-medium'>
-                                <span className='flex gap-2 items-center'><FaUser /> Profile</span>
+        {
+            !searchFocus && <div className='flex gap-3 md:order-2'>
+                <Button onClick={() => { dispatch(toggleTheme()) }} className='w-10 md:w-12 h-10 sm:inline' color={'gray'} pill>
+                    {theme == 'light' ? <FaMoon /> : <FaSun />}
+                </Button>
+                {
+                    user ?
+                        <Dropdown arrowIcon={false} inline label={<Avatar alt='displaypicture' img={user.displayPicture} rounded />} >
+                            <Dropdown.Header>
+                                <span className='block text-sm'>
+                                    {user.username}
+                                </span>
+                                <span className='block text-sm font-semibold truncate'>
+                                    {user.email}
+                                </span>
+                            </Dropdown.Header>
+                            <Link to={'/dashboard?tab=profile'}>
+                                <Dropdown.Item className='text-sm font-medium'>
+                                    <span className='flex gap-2 items-center'><FaUser /> Profile</span>
+                                </Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider />
+                            <Link to={'/dashboard?tab=collections'}>
+                                <Dropdown.Item className='text-sm font-medium'>
+                                    <span className='flex gap-2 items-center'><HiCollection /> Collections</span>
+                                </Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider />
+                            <Dropdown.Item className='text-sm font-medium' onClick={handleSignOut}>
+                                <span className='flex gap-2 items-center'><FaSignOutAlt /> Sign-out</span>
                             </Dropdown.Item>
+                        </Dropdown>
+                        :
+                        <Link to={'/sign-in'}>
+                            <Button outline className='bg-gradient-to-r from-blue-600 via-sky-500 to-teal-300 hover:transition-all delay-100 hover:-translate-y-1' pill>Sign-In</Button>
                         </Link>
-                        <Dropdown.Divider />
-                        <Link to={'/dashboard?tab=collections'}>
-                            <Dropdown.Item className='text-sm font-medium'>
-                                <span className='flex gap-2 items-center'><HiCollection /> Collections</span>
-                            </Dropdown.Item>
-                        </Link>
-                        <Dropdown.Divider />
-                        <Dropdown.Item className='text-sm font-medium' onClick={handleSignOut}>
-                            <span className='flex gap-2 items-center'><FaSignOutAlt /> Sign-out</span>
-                        </Dropdown.Item>
-                    </Dropdown>
-                    :
-                    <Link to={'/sign-in'}>
-                        <Button outline className='bg-gradient-to-r from-blue-600 via-sky-500 to-teal-300 hover:transition-all delay-100 hover:-translate-y-1' pill>Sign-In</Button>
-                    </Link>
-            }
-            <Navbar.Toggle />
-        </div>
+                }
+                <Navbar.Toggle />
+            </div>
+        }
         <Navbar.Collapse>
             <Navbar.Link active={path === '/'} as={'div'}>
                 <Link to={'/'}>
